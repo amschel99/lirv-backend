73d93a84879b5b0f0d087671522c091086fa5909
amschel99 <6290.2020@students.ku.ac.ke>
2024-03-05 16:09:08 +0300

    delete db

--- src/danger/cleardb.ts
+++ src/danger/cleardb.ts
@@ -1,13 +1,17 @@
-import mongoose, {ConnectOptions} from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+import mongoose, { ConnectOptions } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 async function deleteAllCollections(mongoUri: string): Promise<void> {
   try {
-
-    await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions);
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
 
     // Get the list of collection names
-    const collections = await mongoose.connection.db.listCollections().toArray();
+    const collections = await mongoose.connection.db
+      .listCollections()
+      .toArray();
 
     // Delete each collection
     for (const collection of collections) {
@@ -15,20 +19,13 @@ async function deleteAllCollections(mongoUri: string): Promise<void> {
       console.log(`Collection '${collection.name}' deleted.`);
     }
 
-    console.log('All collections deleted successfully.');
+    console.log("All collections deleted successfully.");
   } finally {
     // Close the MongoDB connection
     await mongoose.connection.close();
   }
 }
 
-const connection_url  = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_IP}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`
-
-// Example usage
-if(process.env.REMOTE_MONGO){
-    deleteAllCollections(process.env.REMOTE_MONGO);
-}
-else{
-    throw new Error(`there is no mong url`)
-}
-
+deleteAllCollections(
+  "mongodb+srv://nichlabs:4JsAR0yT34fUO1Iq@nichlabsapp.thoorlb.mongodb.net/?retryWrites=true&w=majority&appName=NichlabsApp"
+);
